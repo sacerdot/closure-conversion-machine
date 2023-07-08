a5f218bb616d8b90b5c74f301d7e01f27a0659a0
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2023-07-08 15:38:40 +0200

    Intermediate language syntax

--- term.ml
+++ term.ml
@@ -8,3 +8,14 @@ struct
    | Tuple of term array
    [@@deriving show {with_path = false}]
 end
+
+module Int =
+struct
+ type term =
+   | Var of string
+   | App of term * term
+   | Proj of int * term
+   | Clos of string array * string array * term * term array (* only vars and values allowed in the array *)
+   | Tuple of term array
+   [@@deriving show {with_path = false}]
+end
