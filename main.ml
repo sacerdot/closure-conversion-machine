814494cd22015c442a7bb82f3b9fd08752882abd
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2023-07-08 15:38:23 +0200

    Allow to exit the program

--- main.ml
+++ main.ml
@@ -2,13 +2,13 @@ open Lexing
 open Term
 
 let rec loop lexer =
-  flush stdout;
-  let _ = match Parser.prog Lexer.read lexer with
-    | Some t ->
-        Printf.printf "%s\n\n" (Source.show_term t);
-    | None -> () in
-  loop lexer
+ flush stdout;
+ match Parser.prog Lexer.read lexer with
+  | Some t ->
+     Printf.printf "%s\n\n" (Source.show_term t);
+     loop lexer
+  | None -> ()
 
 let () =
-  Printf.printf "Enter the term to be evaluated in a single line.\n\n";
-  loop (from_channel stdin)
+ Printf.printf "Enter the term to be evaluated in a single line.\n\n";
+ loop (from_channel stdin)
