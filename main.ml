c0486bdbf953ef5aec0af5e26c76c144b5b26c3c
Claudio Sacerdoti Coen <claudio.sacerdoticoen@unibo.it>
2023-07-08 16:35:46 +0200

    Intermediate to Target

--- main.ml
+++ main.ml
@@ -5,9 +5,11 @@ let rec loop lexer =
  flush stdout;
  match Parser.prog Lexer.read lexer with
   | Some t ->
-     Printf.printf "Source term: %s\n" (Source.show_term t);
+     Printf.printf "Source term:\n%s\n" (Source.show_term t);
      let t = Intermediate.of_source_term t in
-     Printf.printf "Intermediate term: %s\n\n" (Intermediate.show_term t);
+     Printf.printf "Intermediate term:\n%s\n\n" (Intermediate.show_term t);
+     let t = Target.of_intermediate_term t in
+     Printf.printf "Target term:\n%s\n\n" (Target.show_term t);
      loop lexer
   | None -> ()
 
--- term.ml
+++ term.ml
@@ -37,5 +37,36 @@ struct
    | Source.Tuple(ts) -> Tuple(Array.map of_source_term ts)
    | Source.Abs(xs,t) as i ->
       let ys = Array.of_list (Source.StringSet.elements (Source.fv i)) in
-      Clos(ys,xs,of_source_term t,Array.map (fun v -> Var v) ys)
+      Clos(ys, xs, of_source_term t, Array.map (fun v -> Var v) ys)
+end
+
+module Target =
+struct
+ type term =
+   | L of int
+   | S of int
+   | App of term * term
+   | Proj of int * term
+   | Clos of int * int * term * term array (* only vars and values allowed in the array *)
+   | Tuple of term array
+   [@@deriving show {with_path = false}]
+ 
+ let get_pos x a =
+  let rec aux i = if i < 0 then None else if a.(i) = x then Some i else aux (i-1) in
+  aux (Array.length a - 1)
+
+ let rec of_intermediate_term ys xs =
+  function
+   | Intermediate.Var x ->
+      (match get_pos x ys, get_pos x xs with
+        | Some i, None -> L i
+        | None, Some i -> S i
+        | _, _ -> assert false)
+   | Intermediate.App(t,u) -> App(of_intermediate_term ys xs t, of_intermediate_term ys xs u)
+   | Intermediate.Proj(i,t) -> Proj(i, of_intermediate_term ys xs t)
+   | Intermediate.Tuple(ts) -> Tuple(Array.map (of_intermediate_term ys xs) ts)
+   | Intermediate.Clos(zs,ws,t,us) ->
+      Clos(Array.length zs, Array.length ws, of_intermediate_term zs ws t, Array.map (of_intermediate_term ys xs) us)
+ 
+ let of_intermediate_term = of_intermediate_term [||] [||]
 end
